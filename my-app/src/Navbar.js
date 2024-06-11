154de212643c5f66ef2575697193174975f34588
Clair <clair.karanja@student.moringaschool.com>
2024-06-11 17:39:08 +0300

    RUN BUILD

--- my-app/src/Navbar.js
+++ my-app/src/Navbar.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './App.css'; // Import CSS file for styling
 import logo from './logo.jpg'; // Import the logo image
-import { MdPhoneIphone } from 'react-icons/md'; // Import the icon from react-icons
 
 const Navbar = () => {
   const [isVisible, setIsVisible] = useState(false);
@@ -24,26 +23,29 @@ const Navbar = () => {
   return (
     <>
       <div className={`collapse ${isVisible ? 'show' : ''}`} id="navbarToggleExternalContent">
-        <div className="bg-dark p-2"> {/* Reduced padding for smaller size */}
-          <h5 className="text-white h5">Collapsed content</h5> {/* Reduced font size */}
+        <div className="bg-dark p-2">
+          <h5 className="text-white h5">Collapsed content</h5>
           <ul className="navbar-nav">
             <li className="nav-item">
               <a className="nav-link text-white" href="/home">Home</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link text-white" href="/products">OUR Products</a>
+              <a className="nav-link text-white" href="/products">About Us</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link text-white" href="/about">About Us</a>
+              <a className="nav-link text-white" href="/about">Our Products</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link text-white" href="/community">Our Community</a>
+              <a className="nav-link text-white" href="/community">Distributors</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link text-white" href="/testimonials">Testimonial</a>
+              <a className="nav-link text-white" href="/testimonials">Get To Know Our Community</a>
             </li>
             <li className="nav-item">
               <a className="nav-link text-white" href="/dashboard">Dashboard</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link text-white" href="/dashboard">Testimonials</a>
             </li>
           </ul>
         </div>
@@ -61,12 +63,12 @@ const Navbar = () => {
           >
             <span className="navbar-toggler-icon"></span>
           </button>
-          <a className="navbar-brand" href="/">
-            <img src={logo} alt="Logo" className="logo-image" style={{ height: '60px' }} /> {/* Reduced logo size */}
-          </a>
-          <form className="d-flex">
+          <form className="d-flex align-items-center">
+            <a className="navbar-brand m-0 me-2" href="/">
+              <img src={logo} alt="Logo" className="logo-image" style={{ height: '60px' }} /> {/* Reduced logo size */}
+            </a>
             <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-            <button className="btn btn-outline-success" type="submit">Search</button>
+            <button className="btn btn-outline-success me-2" type="submit">Search</button>
           </form>
         </div>
       </nav>
@@ -75,4 +77,3 @@ const Navbar = () => {
 };
 
 export default Navbar;
-
